1132aea2797e8f41d21f4c6f06deeb0280ef09f2
Diegogete <ddrsilva@senacrs.com.br>
2023-09-08 11:26:24 -0300

    Update tela_inicial.js

--- assets/js/tela_inicial.js
+++ assets/js/tela_inicial.js
@@ -1,23 +1,18 @@
 // $('.backdrop').hide()
 //localStorage.clear();
-// $('#modal-video').fadeIn()
-
 $(document).ready(function() {
 
-    // if(localStorage.getItem('a-video-intro')!=='true'){    
-    //     $('#modal-video').fadeIn()
-
-    // }
-    
-    $('#modal-video').fadeIn();
-    $('.btn-video').click(function(){
-        $('#modal-video').fadeIn()
-    })
-    $('.fechar').click(function(){
-        // $('#modal-video').fadeOut()
-        // $("#modal-video iframe").attr("src", $("iframe").attr("src"));
-        // localStorage.setItem('matm1e1-video-intro', 'true')
-    })
+if(localStorage.getItem('a-video-intro')!=='true'){    
+    $('.backdrop').fadeIn()
+}
+$('.btn-video').click(function(){
+    $('.backdrop').fadeIn()
+})
+$('.fechar').click(function(){
+    $('.backdrop').fadeOut()
+    $(".backdrop iframe").attr("src", $(".backdrop iframe").attr("src"));
+    localStorage.setItem('matm1e1-video-intro', 'true')
+})
 
 });
   
