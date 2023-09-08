de4e9684fbf1ae5b29940894d641bb5aa805fbda
Diegogete <ddrsilva@senacrs.com.br>
2023-09-08 14:28:06 -0300

    ajuste video mob

--- assets/js/tela_inicial.js
+++ assets/js/tela_inicial.js
@@ -1,18 +1,32 @@
 // $('.backdrop').hide()
 //localStorage.clear();
-$(document).ready(function() {
+// $(document).ready(function() {
 
-if(localStorage.getItem('a-video-intro')!=='true'){    
-    $('.backdrop').fadeIn()
-}
-$('.btn-video').click(function(){
-    $('.backdrop').fadeIn()
+//     if(localStorage.getItem('a-video-intro')!=='true'){    
+//     $('.backdrop').fadeIn()
+//     }
+//     $('.btn-video').click(function(){
+//         $('.backdrop').fadeIn()
+//     })
+//     $('#fechar').click(function(){
+//         $('.backdrop').fadeOut()
+//         $(".backdrop iframe").attr("src", $(".backdrop iframe").attr("src"));
+//         localStorage.setItem('matm1e1-video-intro', 'true')
+//     })
+
+// });
+window.addEventListener('load', function(){ 
+    $('.preloader').fadeOut(300);
 })
-$('.fechar').click(function(){
+
+    if(localStorage.getItem('matm1e1-video-intro')!=='true'){    
+    $('.backdrop').fadeIn()
+    }
+$('#fechar-video').click(function(){
     $('.backdrop').fadeOut()
     $(".backdrop iframe").attr("src", $(".backdrop iframe").attr("src"));
     localStorage.setItem('matm1e1-video-intro', 'true')
 })
-
-});
-  
+$('.btVideo').click(function(){
+    $('.backdrop').fadeIn()
+})
