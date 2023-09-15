a98aa7799c27aafce3d43d02cf0260d22c50cd1d
luiz-brancher <lppbrancher@senacrs.com.br>
2023-09-15 19:37:12 -0300

    ODA embalagens

---
+++ objeto_embalagem/assets/js/script.js
@@ -0,0 +1,45 @@
+$('.pqjo').click(function(){
+    if($('.pqjo').hasClass('active')){
+        $('.pqjo').attr('src', '../../objeto_embalagem/assets/img/pao_queijo.png')
+        $('.pqjo').removeClass('active')
+    } else {
+        $('.pqjo').addClass('active')
+        $('.pqjo').attr('src', '../../objeto_embalagem/assets/img/pao_quijo_lupa.png')
+    }
+})
+$('.pizza').click(function(){
+    if($('.pizza').hasClass('active')){
+        $('.pizza').attr('src', '../../objeto_embalagem/assets/img/pizza.png')
+        $('.pizza').removeClass('active')
+    } else {
+        $('.pizza').addClass('active')
+        $('.pizza').attr('src', '../../objeto_embalagem/assets/img/pizza_lupa.png')
+    }
+})
+$('.sorvete').click(function(){
+    if($('.sorvete').hasClass('active')){
+        $('.sorvete').attr('src', '../../objeto_embalagem/assets/img/sorvete.png')
+        $('.sorvete').removeClass('active')
+    } else {
+        $('.sorvete').addClass('active')
+        $('.sorvete').attr('src', '../../objeto_embalagem/assets/img/sorvete_lupa.png')
+    }
+})
+$('.frango').click(function(){
+    if($('.frango').hasClass('active')){
+        $('.frango').attr('src', '../../objeto_embalagem/assets/img/frango.png')
+        $('.frango').removeClass('active')
+    } else {
+        $('.frango').addClass('active')
+        $('.frango').attr('src', '../../objeto_embalagem/assets/img/frango_lupa.png')
+    }
+})
+$('.lasanha').click(function(){
+    if($('.lasanha').hasClass('active')){
+        $('.lasanha').attr('src', '../../objeto_embalagem/assets/img/lasanha.png')
+        $('.lasanha').removeClass('active')
+    } else {
+        $('.lasanha').addClass('active')
+        $('.lasanha').attr('src', '../../objeto_embalagem/assets/img/lasanha_lupa.png')
+    }
+})
