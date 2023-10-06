84571684068a8f4d802b3679eb1521b0363f05e9
RenataLimaTED <relferreira@senacrs.com.br>
2023-10-06 12:15:42 -0300

    inclusao do objeto reta numerica e botao para iniciar jogo

--- conteudo/01_historia_numeros/assets/js/geral.js
+++ conteudo/01_historia_numeros/assets/js/geral.js
@@ -1,11 +1,11 @@
 // caixas expansiveis
-$('.caixa-container button.btpluscaixa').click(function(){
+$('.caixa-container button.btpluscaixa').click(function () {
   let caixa = $(this).prev()
-  if(caixa.hasClass('aberta')){
-      caixa.removeClass('aberta')
-      $(this).css('background-image', 'url("assets/img/bt_mais2.png")')
+  if (caixa.hasClass('aberta')) {
+    caixa.removeClass('aberta')
+    $(this).css('background-image', 'url("assets/img/bt_mais2.png")')
   } else {
-      caixa.addClass('aberta')
-      $(this).css('background-image', 'url("assets/img/bt_menos2.png")')
+    caixa.addClass('aberta')
+    $(this).css('background-image', 'url("assets/img/bt_menos2.png")')
   }
 })
