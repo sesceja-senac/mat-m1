8f9c8bf1a56b46fb3bfab0ee2275bfe96baa1478
Paulo-SenacRS <prjunior@senacrs.com.br>
2023-10-24 17:12:15 -0300

    ajustes

--- conteudo/01_historia_numeros/assets/js/geral.js
+++ conteudo/01_historia_numeros/assets/js/geral.js
@@ -9,3 +9,23 @@ $('.caixa-container button.btpluscaixa').click(function () {
     $(this).css('background-image', 'url("assets/img/bt_menos2.png")')
   }
 })
+
+
+//---- Troca de BGs conforme página ----\\
+jQuery("document").ready(function($){
+
+  var pg_number = parseInt(window.location.href.split('?')[1].split('=')[1]);
+
+  //alert('Página: '+pg_number);
+
+  if(pg_number == 1){
+    document.body.style.backgroundImage = "url('assets/imgAjuste/versionamento/bg_laranja.png'), linear-gradient(#EAC18F, #D3974F)";
+  }else if(pg_number == 2){
+    document.body.style.backgroundImage = "url('assets/imgAjuste/versionamento/bg_verde.png'), linear-gradient(#7DDFE2, #004F53)";
+  }else if(pg_number == 3){
+    document.body.style.backgroundImage = "url('assets/imgAjuste/versionamento/bg_laranja.png'), linear-gradient(#D3974F, #EAC18F)";
+  }else if(pg_number == 4){
+    document.body.style.backgroundImage = "url('assets/imgAjuste/versionamento/bg_verde.png'), linear-gradient(#004F53, #7DDFE2)";
+  }
+
+});
